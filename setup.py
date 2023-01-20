a157b546dfdacb9dcedab98bbb7ddbaeaee0410d
CoolPlayLin <CoolPlayLin@qq.com>
2023-01-20 17:21:00 +0800

    更新：升级打包文件

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as file:
+    README = file.read()
+
 setup(
     name="molick",
     version="0.0.0",
     author="yourmoln",
     author_email="yourmoln@outlook.com",
     description="Hacker's exclusive library",
+    long_description=README,
+    long_description_content_type='text/markdown',
     url="https://github.com/molick-tech/molick", 
-
+    project_urls={
+                "Bug Report": "https://github.com/molick-tech/molick/issues/new"
+            },
     packages=find_packages(),
 
     classifiers = [
